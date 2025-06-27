ca9492e9ae055c36d3e97cf03c9ca264af489480
ParedesGab <paredesgab@gmail.com>
2025-06-27 10:27:04 +0000

    Add page_summary information

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_house_sales_price_study import page_house_sales_price_study_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_predict_sales_price import page_predict_sales_price_body
 
-from app_pages.page_prospect import page_prospect_body
+#from app_pages.page_prospect import page_prospect_body
 
-from app_pages.page_predict_churn import page_predict_churn_body
-from app_pages.page_predict_tenure import page_predict_tenure_body
-from app_pages.page_cluster import page_cluster_body
 
-app = MultiPage(app_name= "Churnometer") # Create an instance of the app 
+
+
+
+app = MultiPage(app_name= "Homelytics") # Create an instance of the app 
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Customer Base Churn Study", page_house_sales_price_study_body)
+app.add_page("House Sales Price Study", page_house_sales_price_study_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+app.add_page("ML: Prospect House Sales Price", page_predict_sales_price_body)
 
-app.add_page("Prospect Churnometer", page_prospect_body)
+#app.add_page("Prospect Churnometer", page_prospect_body)
 
-app.add_page("ML: Prospect Churn", page_predict_churn_body)
-app.add_page("ML: Prospect Tenure", page_predict_tenure_body)
-app.add_page("ML: Cluster Analysis", page_cluster_body)
+
 
 app.run() # Run the  app
--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -6,19 +6,16 @@ def page_summary_body():
 
     # text based on README file - "Dataset Content" section
     st.info(
-        f"**Project Terms & Jargon**\n"
-        f"* A **customer** is a person who consumes your service or product.\n"
-        f"* A **prospect** is a potential customer.\n"
-        f"* A **churned** customer is a user who has stopped using your product or service.\n "
-        f"* This customer has a **tenure** level, the number of months this person " 
-        f"has used our product/service.\n\n"
+		f"**Project Terms & Jargon**\n"
+		f"* A **house** refers to an individual residential unit in Ames, Iowa.\n"
+		f"* A **house attribute** is a characteristic of the house, such as square footage, quality rating, or year built.\n"
+		f"* The **Sale Price** is the actual price a property sold for, which our model aims to predict.\n"
+		f"* An **inherited property** refers to one of the 4 inherited houses for which the client requires a precise sale price prediction.\n"
         f"**Project Dataset**\n"
-        f"* The dataset represents a **customer base from a Telco company** "
-        f"containing individual customer data on the products and services "
-        f"(like internet type, online security, online backup, tech support), "
-        f"account information (like contract type, payment method, monthly charges) "
-        f"and profile (like gender, partner, dependents).")
-
+		f"* The dataset represents **housing records from Ames, Iowa, indicating house profile**"
+		f"* A house profile contains information on individual house attributes (e.g., Floor Area, Basement Finished Area, Garage Area, Garage Finish, Overall Quality, Kitchen Quality, Lot Size, Porch Area, Wood Deck, Year Built, Remodel Date, etc.) and their respective **Sale Prices**.\n"
+		f"* The houses in this dataset were constructed **between 1872 and 2010**.\n")
+		
     # Link to README file, so the users can have access to full project documentation
     st.write(
         f"* For additional information, please visit and **read** the "
@@ -28,12 +25,7 @@ def page_summary_body():
     # copied from README file - "Business Requirements" section
     st.success(
         f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in understanding the patterns from the customer base "
-        f"so that the client can learn the most relevant variables that are correlated to a "
-        f"churned customer.\n"
-        f"* 2 - The client is interested in determining whether or not a given prospect will churn. "
-        f"If so, the client is interested to know when. In addition, the client is "
-        f"interested in learning from which cluster this prospect will belong in the customer base. "
-        f"Based on that, present potential factors that could maintain and/or bring  "
-        f"the prospect to a non-churnable cluster."
-        )
+        f"* 1 - The client is interested in understanding the patterns from the housing records from Ames, Iowa"
+        f" so that the client can learn the most relevant variables (i.e., house attributes) that are correlated to a house sales price.\n"
+        f"* 2 - The client is interested in predicting the house sale prices from her 4 inherited houses"
+        f" so, the client can maximize the sales price for her inherited properties.")
