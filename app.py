10985adf4ee5d3f96d79a820ef9165eb0f583c8f
ParedesGab <paredesgab@gmail.com>
2025-06-27 08:57:02 +0000

    Create pages: project hypothesis and predict_sales_price

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_house_sales_price_study import page_house_sales_price_study_body
-from app_pages.page_prospect import page_prospect_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+
+from app_pages.page_prospect import page_prospect_body
+
 from app_pages.page_predict_churn import page_predict_churn_body
 from app_pages.page_predict_tenure import page_predict_tenure_body
 from app_pages.page_cluster import page_cluster_body
@@ -15,11 +17,10 @@ app = MultiPage(app_name= "Churnometer") # Create an instance of the app
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Customer Base Churn Study", page_house_sales_price_study_body)
-
-
+app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 
 app.add_page("Prospect Churnometer", page_prospect_body)
-app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+
 app.add_page("ML: Prospect Churn", page_predict_churn_body)
 app.add_page("ML: Prospect Tenure", page_predict_tenure_body)
 app.add_page("ML: Cluster Analysis", page_cluster_body)
