b5658e2991d6dc219b76acbec7b68056a29f8810
ParedesGab <paredesgab@gmail.com>
2025-07-02 17:38:20 +0000

    Complete page 5

--- app_pages/page5_technical_overview.py
+++ app_pages/page5_technical_overview.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from src.data_management import load_housing_data, load_pkl_file
-from src.machine_learning.evaluate_reg import regression_performance, regression_evaluation_plots
+from src.data_management import load_pkl_file
+from src.machine_learning.evaluate_reg import regression_performance
 
 
 def page_display_technical_overview():
-
     # load House Sales Price pipeline files
-	
     version = 'v1'
     saleprice_pipe = load_pkl_file(
-        f"outputs/ml_pipeline/predict_saleprice/{version}/best_features_regressor_pipeline.pkl")
+        f"outputs/ml_pipeline/predict_saleprice/{version}/"
+        f"best_features_regressor_pipeline.pkl")
 		
     saleprice_feat_importance = plt.imread(
-        f"outputs/ml_pipeline/predict_saleprice/{version}/features_importance.png")
+        f"outputs/ml_pipeline/predict_saleprice/{version}/"
+        f"features_importance.png")
     X_train = pd.read_csv(
         f"outputs/ml_pipeline/predict_saleprice/{version}/X_train.csv")
     X_test = pd.read_csv(
@@ -26,14 +26,20 @@ def page_display_technical_overview():
     
     st.write("")
     st.write("")
+
     st.write("### ML Pipeline: Predict House Sale Price")
     st.write("")
+
     # display pipeline training summary conclusions
     st.success(
-        f"* A Regressor model was used to predict house sale prices in Ames, Iowa.\n"
-        f"* The regressor performance met the client requirement: "
-        f" an R2 score of **at least 0.75 on both, the train and test set.**\n"
-        f"* The regressor performance metric was CHANGE on both sets.\n")
+        f"* A Regressor model was used to predict house sale prices in "
+        f"Ames, Iowa.\n"
+        f"* The regressor performance met the client's requirement of an: "
+        f"R2 score of **at least 0.75 on both, the train and test set.**\n"
+        f"* Namely, the regressor performance metric was for:\n "
+        f"  * Train set -  R2 Score: 0.959 \n"
+        f"  * Test set - R2 score: 0.778 \n"
+        )
     st.write("---")
 
     # show pipeline steps
@@ -51,4 +57,3 @@ def page_display_technical_overview():
     st.write("### Pipeline Performance - Model Evaluation")
     st.write("")
     regression_performance(X_train, y_train, X_test, y_test, saleprice_pipe)
-    
