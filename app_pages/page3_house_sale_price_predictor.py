b3ebf3dcc10199825c76b2a3502de0377c6dff6d
ParedesGab <paredesgab@gmail.com>
2025-07-02 16:53:28 +0000

    Complete page 3

--- app_pages/page3_house_sale_price_predictor.py
+++ app_pages/page3_house_sale_price_predictor.py
@@ -5,32 +5,32 @@ from src.data_management import (
     load_inherited_housing_data, 
     load_pkl_file
     )
-#from src.machine_learning.evaluate_reg import (
-# regression_performance, 
-# regression_evaluation
-#)
 from src.machine_learning.predictive_analytics import predict_saleprice
 
 
 def page_predict_house_sale_price_body():
-
     # load predict tenure files
     version = 'v1'
     saleprice_pipe = load_pkl_file(
-        f"outputs/ml_pipeline/predict_saleprice/{version}/best_features_regressor_pipeline.pkl")
-    best_features = (pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/{version}/X_train.csv")
-                       .columns
-                       .to_list()
+        f"outputs/ml_pipeline/predict_saleprice/{version}/"
+        f"best_features_regressor_pipeline.pkl")
+    best_features = (pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/"
+                                 f"{version}/X_train.csv").columns.to_list()
                        )
     inherited_houses = load_inherited_housing_data()
 
+    st.write("")
+    st.write("")
     st.write("### Interface to Predict House Sale Price")
-    st.info(
-        f"* This interface answers Business requirement 2"
-        f"* The client is interested in predicting the house sale prices from her 4 inherited houses"
-        f" so, the client can maximize the sales price for her inherited properties."
+    st.write("")
+    st.success(
+        f"This page answers **Business requirement 2:**\n\n"
+        f"* The client is interested in predicting the house sale prices "
+        f"from her 4 inherited houses so, that the client can maximize the "
+        f"sales price for her inherited properties.\n"
     )
-    with st.expander("ℹ️ Visualize the 4 inherited Houses attributes"):
+    with st.expander("ℹ️ Visualize the 4 inherited Houses attributes, and" \
+    "predict sales price"):
         sale_price_inh_houses = predict_saleprice(
         inherited_houses, best_features, saleprice_pipe)
 
@@ -38,20 +38,33 @@ def page_predict_house_sale_price_body():
         name="Predicted Sale Price")
 
         best_feat_inh_houses = inherited_houses[best_features]
+
         merged_display_df = pd.concat([best_feat_inh_houses, predicted_price_df], axis=1)
         st.write(merged_display_df)
 
-    # --- Code for calculating and displaying the sum of predicted prices ---
     st.write("---") 
+
+    # Calculate and display the sum of predicted prices
     st.subheader("Total Predicted Sale Price for Inherited Houses:")
 
-    # Calculate the sum of all predicted sale prices
     total_predicted_price = sale_price_inh_houses.sum()
+
     st.metric(label="Total Predicted Price for the 4 Inherited Houses:",
                value=f"${total_predicted_price:,.2f}")
-
-
+    
     st.write("---")
+
+    # Predict a Sale Price for a House in Ames, Iowa
+    st.info(f"PREDICT A SALE PRICE FOR A HOUSE IN AMES, IOWA!\n\n"
+               f"* The four interactive widgets provided below "
+               f"correspond to the house attributes OverallQual, GarageArea, "
+               f"TotalBsmtSF, and YearRemodAdd. \n"
+               f"* These specific attributes were chosen for the widgets "
+               f"because the machine learning regression model identified "
+               f"them as its most influential or **best features** for "
+               f"predicting house prices.\n")
+    st.write("")
+    st.write("")
 
     # Generate Live Data
     X_live = DrawInputsWidgets()
@@ -85,15 +98,15 @@ def DrawInputsWidgets():
         feature = "OverallQual"
         st_widget = st.number_input(
             label=feature,
-            min_value=1,   # Starts at 1
-            max_value=10,  # Maximum value is 10
-            value=1,       # Initial value displayed is 1
-            step=1 
+            min_value=1,
+            max_value=10,
+            value=1,
+            step=1
         )
     X_live[feature] = st_widget
 
     with col2:
-        feature = "TotalBsmtSF"
+        feature = "GarageArea"
         st_widget = st.number_input(
             label=feature,
             min_value=df[feature].min()*percentageMin,
@@ -102,17 +115,16 @@ def DrawInputsWidgets():
     X_live[feature] = st_widget
 
     with col3:
-        feature = "2ndFlrSF"
+        feature = "TotalBsmtSF"
         st_widget = st.number_input(
             label=feature,
             min_value=df[feature].min()*percentageMin,
             max_value=df[feature].max()*percentageMax,
-            value=df[feature].median()
         )
     X_live[feature] = st_widget
 
     with col4:
-        feature = "GarageArea"
+        feature = "YearRemodAdd"
         st_widget = st.number_input(
             label=feature,
             min_value=df[feature].min()*percentageMin,
